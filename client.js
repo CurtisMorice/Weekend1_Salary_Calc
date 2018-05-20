2306cc478589dba2252ac0ecf85028b0939387d1
Curtis Morice <curtismorice@gmail.com>
2018-05-20 12:00:34 -0500

    tables are fixed

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ class Employee {
 
     }
 }
+let employees = [];
 
 function readyNow() {
     console.log('JQ');
@@ -25,11 +26,43 @@ function readyNow() {
 //create a function that calculates the total salaries of the employees
 function addEmployee() {
     console.log('In addEmployee');
-    let newEmployee = new Employee('ffff', 'dfsd', 445, 'mand', 45354);
-    console.log(newEmployee);
-    //get user input
+    //$('').empty();
+    let newEmployee = new Employee($('#firstName').val(), $('#lastName').val(), $('#employeeId').val(), $('#title').val(), $('#annualSalary').val());
+    employees.push(newEmployee);
+    console.log(employees);
+    displayEmployees();
     //create spread of employee info
     //push into an array
 
 
 } // add Employee
+function displayEmployees() {
+    console.log("in displayEmployees");
+    let el = $('#outputInfo');
+    el.empty();
+    for (employee of employees) {
+        let outputString = '<td>';
+
+        outputString += employee.firstName;
+        outputString += '</td>';
+
+        outputString += '<td>';
+        outputString += employee.lastName;
+        outputString += '</td>';
+
+        outputString += '<td>';
+        outputString += employee.employeeId;
+        outputString += '</td>';
+
+        outputString += '<td>';
+        outputString += employee.title;
+        outputString += '</td>';
+
+        outputString += '<td>';
+        outputString += employee.annualSalary;
+        outputString += '</td>';
+
+        el.append(outputString);
+
+    } // end for
+}
